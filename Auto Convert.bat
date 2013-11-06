b75875dc202afd9a7db0ac576207b309824aa03b
Elliot Huffman <thelich2@gmail.com>
2013-11-05 20:53:23 -0500

    Initial Release
    v1 of the ARF converter is now complete.

--- Auto Convert.bat
+++ Auto Convert.bat
@@ -1,75 +1,68 @@
-:start
 @echo off
-cls
-cd /d %~dp0
-goto source
-
-rem This sets up the working folder. I usually do this because I need to but I am 
-rem not sure about it this time...
-
-:source
 echo Please select the folder with the ARF files in it.
-set /p source=E.G. C:\Users\Elliot\Desktop\toconvert:
-goto dest
-
+set /p source=E.G. C:\Users\Elliot\Desktop\HAOH:
 
 rem This sets the folder location for the ARF files.
 rem I am going to add a menu for simplicity sake later.
 
 
-:dest
 echo Please select where the mp4 files should appear.
 set /p dest=E.G. C:\Users\Elliot\Desktop\Converted:
-goto cfgname
 
 
 rem This sets the output folder for the converted files.
 rem Again I will make a menu in the future.
 
 
-:cfgname
+setlocal
+set cd=%source%
+cd %cd%
+for %%a in ("%cd%\*.arf") do call:MakeCFG "%%~a" "%dest%" "%source%"
+for %%a in ("%cd%\*.cfg") do c:\programdata\webex\webex\500\nbrplay.exe -Convert %%~a
+goto end
 
 
-goto createcfg
+rem lists the contents of %cd% and puts the list in %%a.
+rem Then processes the list (%a) and runs the call command for each entry while passing
+rem the preselected destination and source to the config file that is created.
 
-rem this sets the name of the CFG that will be created for the ARF file
-
-
-:createcfg
-echo [Console] >> %MP4%.cfg
-echo inputfile=%source%\1.arf >> MP4.cfg
-echo media=MP4 >> MP4.cfg
-echo showui=1 >> MP4.cfg
-echo [UI] >> MP4.cfg
-echo chat=1 >> MP4.cfg
-echo qa=0 >> MP4.cfg
-echo largeroutline=1 >> MP4.cfg
-echo [MP4] >> MP4.cfg
-echo outputfile=%dest%\1.mp4 >> MP4.cfg
-echo width=1024 >> MP4.cfg
-echo height=768 >> MP4.cfg
-echo framerate=10 >> MP4.cfg
+rem After that is done it then looks and makes a list of all the config files in the %cd% folder, again as %a.
+rem Then it runs the NBRPLAY.exe -convert %%~a to process all of the config files in the %cd% folder then convert the specified ARF file (in the CFG file)
 
 
-rem This makes the config file that is required by the player to convert
+:MakeCFG
+setlocal
+set "MP4=%~n1"
+set "source=%~dp1"
+set "filename=%~n1"
 
-:convert
-cd C:\programdata\webex\webex\500
-nbrplay.exe -Convert "%cfg%"
+(
+ECHO([Console]
+ECHO(inputfile=%source%%filename%.arf
+ECHO(media=MP4
+ECHO(showui=1
+ECHO([UI]
+ECHO(chat=1
+ECHO(qa=0
+ECHO(largeroutline=1
+ECHO([MP4]
+ECHO(outputfile=%dest%\%filename%.mp4
+ECHO(width=1024
+ECHO(height=768
+ECHO(framerate=10
+)>"%MP4%.cfg"
+exit /b
+
+rem Above is the CFG file template used to apply to the ARF's CFG file.
+
+
 
 :end
-echo Thanks for using Elliot Labs Auto Converter.
-echo for feature requests please email: elliot-labs@live.com
-pause | echo Press any key to exit...
-exit
-
-:preloop
-set num=0
-goto loop
-
-:loop
 cls
-echo %num%
-pause
-set /a num+=1
-if %num%==6 (goto end) ELSE goto loop
+echo Thank you for using the Elliot Labs ARF converter.
+echo Special thanks to HuffDaddy for coding help.
+echo For feature requests please email Elliot at elliot-labs@live.com
+pause | echo Press any key to exit...
+exit /b
+
+rem Exits the program on the user's input while giving the credits.
