43934fc0381383248695d5f0f393fd3f9f588642
Elliot Huffman <thelich2@gmail.com>
2015-03-09 16:26:18 -0400

    New features
    Add the complete program and documentation (REMs).

--- Easy MP4.bat
+++ Easy MP4.bat
@@ -4,6 +4,9 @@ color 2e
 cls
 cd /d %~dp0
 if not EXIST C:\programdata\webex\webex\500\nbrplay.exe goto nonbr
+goto precfg
+
+rem Checks if the required program is installed, turns off the output of commands and sets the color of the terminial. After it has completed the above tasks it routs the user to the main menu
 
 :nonbr
 cls
@@ -22,14 +25,15 @@ start www.webex.com/play-webex-recording.html
 pause
 goto end
 
+rem The above is displayed if you do not have the Networking Recording Player (nbrplayer)
+rem installed. It displays a link to the download page to the NBR tool.
+
 :precfg
 cls
 setlocal
-set cd=%source%
-cd /d %cd%
-for %%a in ("%cd%\*.arf") do call:MakeMP4CFG "%%~a" "%dest%" "%source%"
+for %%a in ("%cd%\*.arf") do call:MakeMP4CFG "%%~a" "%dest%\Converted" "%cd%"
 for %%a in ("%cd%\*.cfg") do set /a count +=1
-for %%a in ("%cd%\*.cfg") do call:countnconvert "%%~a" "%source%" "%count%" & set /a count -=1
+for %%a in ("%cd%\*.cfg") do call:countnconvert "%%~a" "%cd%" "%count%" & set /a count -=1
 del *.cfg
 goto end
 
@@ -66,9 +70,25 @@ ECHO(framerate=10
 )>"%MP4%.cfg"
 exit /b
 
+rem Above is the CFG file template used to create the ARF's CFG file.
+
 :countnconvert
 setlocal
 cls
 echo %count% files remaining... This may take some time ;)
 c:\programdata\webex\webex\500\nbrplay.exe -Convert "%~dp1\%~n1.cfg"
 exit /b
+
+rem This converts the file based upon the imputed CFG file that was created in one of the previous steps.
+rem This also displays the current files that are left for conversion.
+
+:end
+cls
+echo Thank you for using the Elliot Labs ARF converter.
+echo Special thanks to HuffDaddy for coding help.
+echo For feature requests please email Elliot at elliot-labs@live.com
+echo.
+pause | echo Press any key to exit...
+exit /b
+
+rem Exits the program on the user's input while giving the credits.
