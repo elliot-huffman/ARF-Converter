92a58c1cd786f8a0b881932b11b7939e6cc93a22
Elliot Huffman <thelich2@gmail.com>
2015-04-10 12:56:15 -0400

    completed mp4 options
    Completed the mp4 options and added the framework for the wmv options.

--- Auto Convert.py
+++ Auto Convert.py
@@ -166,6 +166,11 @@ def main_menu():
         options_menu()
     elif me_main_menu == 5:
         exit_program(True)
+    else:
+        clear_screen()
+        print("Please enter a valid number from 1 to 5!")
+        input("\n Press Enter/Return to continue...")
+        main_menu()
 
 
 # Creates the main menu for the user to navigate.
@@ -266,7 +271,7 @@ def options_menu():
     print("4. Global Options (applies to all formats)")
     print("\n5. Restore default settings")
     print("\n6. Go back to the main menu")
-    me_options_menu = input("\nEnter your selection here (1-5)")
+    me_options_menu = input("\nEnter your selection here (1-6)")
     if me_options_menu == 1:
         mp4_options_menu()
     elif me_options_menu == 2:
@@ -279,24 +284,164 @@ def options_menu():
         restore_default_settings()
     elif me_options_menu == 6:
         main_menu()
+    else:
+        clear_screen()
+        print("Please enter a valid number from 1 to 6!")
+        input("\n Press Enter/Return to continue...")
+        options_menu()
 
 
 # Creates the options menu for the user to navigate.
 
 
 def mp4_options_menu():
-    print("MP4 Files have 4 configurable options (at the moment)\n\n1. Toggle Chat Window\n2. Toggle WebCam Video")
-    print("3. Toggle Largeroutline\n4. Change Framerate")
-    me_mp4_options_menu = input("\nPlease enter 1-4 and press Enter/Return: ")
-    
-
+    clear_screen()
+    print("MP4 Files have 4 configurable options (at the moment)\n\n1. Toggle Chat Window\n2. Toggle Q&A Box")
+    print("3. Toggle Largeroutline\n4. Change Framerate\n\n5. Go back to the main options menu.")
+    me_mp4_options_menu = input("\nPlease enter 1-5 and press Enter/Return: ")
+    if me_mp4_options_menu == 1:
+        mp4_toggle_chat()
+    elif me_mp4_options_menu == 2:
+        mp4_toggle_qa()
+    elif me_mp4_options_menu == 3:
+        mp4_toggle_largeroutline()
+    elif me_mp4_options_menu == 4:
+        mp4_change_framerate()
+    elif me_mp4_options_menu == 5:
+        options_menu()
+    else:
+        clear_screen()
+        print("Please enter a valid number from 1 to 5!")
+        input("\n Press Enter/Return to continue...")
+        mp4_options_menu()
 
 
 # Lists settings available for the MP4 file format
 
 
+def mp4_toggle_chat():
+    clear_screen()
+    print("The current chat toggle is set to: %s\n Would you like to toggle this setting?" % m_ui_chat)
+    print("Press Y to toggle the setting. Leave it blank to do nothing.")
+    me_mp4_toggle_chat = input("Press Enter/Return when you are ready to continue: ")
+    if me_mp4_toggle_chat.lower() == "y":
+        if m_ui_chat == 1:
+            m_ui_chat = 0
+            global m_ui_chat
+            clear_screen()
+            print("The chat toggle is now set to: %s" % m_ui_chat)
+            input("Press Enter/Return to continue...")
+            mp4_options_menu()
+        else:
+            m_ui_chat = 1
+            global m_ui_chat
+            clear_screen()
+            print("The chat toggle is now set to: %s" % m_ui_chat)
+            input("Press Enter/Return to continue...")
+            mp4_options_menu()
+    else:
+        mp4_options_menu()
+
+
+def mp4_toggle_qa():
+    clear_screen()
+    print("The current Q&A toggle is set to: %s\n Would you like to toggle this setting?" % m_ui_qa)
+    print("Press Y to toggle the setting. Leave it blank to do nothing.")
+    me_mp4_toggle_qa = input("Press Enter/Return when you are ready to continue: ")
+    if me_mp4_toggle_qa.lower() == "y":
+        if m_ui_qa == 1:
+            m_ui_qa = 0
+            global m_ui_qa
+            clear_screen()
+            print("The Q&A toggle is now set to: %s" % m_ui_qa)
+            input("Press Enter/Return to continue...")
+            mp4_options_menu()
+        else:
+            m_ui_qa = 1
+            global m_ui_qa
+            clear_screen()
+            print("The Q&A toggle is currently set to: %s" % m_ui_qa)
+            input("Press Enter/Return to continue...")
+            mp4_options_menu()
+    else:
+        mp4_options_menu()
+
+
+def mp4_toggle_largeroutline():
+    clear_screen()
+    print("The current Largeroutline toggle is set to: %s\n Would you like to toggle this setting?" % m_ui_largeroutline)
+    print("Press Y to toggle the setting. Leave it blank to do nothing.")
+    me_mp4_toggle_largeroutline = input("Press Enter/Return when you are ready to continue: ")
+    if me_mp4_toggle_largeroutline.lower() == "y":
+        if m_ui_largeroutline == 1:
+            m_ui_largeroutline = 0
+            global m_ui_largeroutline
+            clear_screen()
+            print("The largeroutline toggle is now set to: %s" % m_ui_largeroutline)
+            input("Press Enter/Return to continue...")
+            mp4_options_menu()
+        else:
+            m_ui_largeroutline = 1
+            global m_ui_largeroutline
+            clear_screen()
+            print("The largeroutline toggle is now set to: %s" % m_ui_largeroutline)
+            input("Press Enter/Return to continue...")
+            mp4_options_menu()
+    else:
+        mp4_options_menu()
+
+
+def mp4_change_framerate():
+    clear_screen()
+    print("The current framerate is set to: %sFPS.\nWould you like to change this setting?" % m_framerate)
+    print("Enter a number above 0 (the recommended range is 1 to 10) to toggle the setting.")
+    me_mp4_change_framerate = input("Leave it blank to do nothing. Press Enter/Return when you are ready to continue: ")
+    if me_mp4_change_framerate > 0:
+        m_framerate = me_mp4_change_framerate
+        global m_framerate
+        clear_screen()
+        print("The framerate is now set to: %sFPS" % m_framerate)
+        input("Press Enter/Return to continue...")
+        mp4_options_menu()
+    else:
+        mp4_options_menu()
+
+
 def wmv_options_menu():
-    pass
+    clear_screen()
+    print("WMV Files have 10 configurable options (at the moment)\n\n1. Toggle PCAudio setting\n2. Toggle Chat Box")
+    print("3. Toggle Webcam Video\n4. Toggle Largeroutline setting\n5. Change the video codec")
+    print("6. Change the audio codec\n7. Alter the Videoformat setting\n8. Alter the Audioformat setting")
+    print("9. Change the video key frames (framerate)\n10. Change the maxstream (bitrate)")
+    print("\n\n11. Go back to the main options menu.")
+    me_mp4_options_menu = input("\nPlease enter 1-11 and press Enter/Return: ")
+    if me_mp4_options_menu == 1:
+        wmv_toggle_pcaudio()
+    elif me_mp4_options_menu == 2:
+        wmv_toggle_chat_box()
+    elif me_mp4_options_menu == 3:
+        wmv_toggle_webcam_video()
+    elif me_mp4_options_menu == 4:
+        wmv_toggle_largeroutline()
+    elif me_mp4_options_menu == 5:
+        wmv_change_videocodec()
+    elif me_mp4_options_menu == 6:
+        wmv_change_audiocodec()
+    elif me_mp4_options_menu == 7:
+        wmv_alter_videoformat()
+    elif me_mp4_options_menu == 8:
+        wmv_alter_audioformat()
+    elif me_mp4_options_menu == 9:
+        wmv_change_keyframes()
+    elif me_mp4_options_menu == 10:
+        wmv_change_maxstream()
+    elif me_mp4_options_menu == 11:
+        options_menu()
+    else:
+        clear_screen()
+        print("Please enter a valid number from 1 to 11!")
+        input("\n Press Enter/Return to continue...")
+        mp4_options_menu()
 
 
 # Lists settings available for the WMV file format
@@ -327,9 +472,12 @@ def exit_program(friendly):
     clear_screen()
     if friendly:
         print("Thank you for using this script, I hope you enjoyed it!")
-    else:
+    elif not friendly:
         print("Thank you for considering this product! If you have any issues please email thelich2@gmail.com or")
-        print("file an issue on GitHub\n\nThanks!")
+        print("file an issue on https://github.com/elliot-labs/ARF-Converter\n\nThanks!")
+    else:
+        print("You have triggered an exit that the programmer has not foreseen.")
+        print("Please report this to thelich2@gmail.com or https://github.com/elliot-labs/ARF-Converter")
     exit()
 
 
