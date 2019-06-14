db0153cf3de366145a56de113490e55e460e5c6f
Elliot Huffman <ehuffman@elliot-labs.com>
2019-06-14 11:52:07 -0400

    Initial powershell skeleton
    Build an initial skeleton for a powershell version of the converter.

---
+++ Covert-ARF.ps1
@@ -0,0 +1,77 @@
+<#
+.SYNOPSIS
+    Converts ARF files to the specified format.
+.DESCRIPTION
+    Long description
+.EXAMPLE
+    PS C:\> <example usage>
+    Explanation of what the example does
+.INPUTS
+    Inputs (if any)
+.OUTPUTS
+    Output (if any)
+.NOTES
+    General notes
+#>
+param(
+# todo
+)
+
+function Test-Prerequisite {
+    <#
+    .SYNOPSIS
+        Checks if the pre-requisites are installed on the machine
+    .DESCRIPTION
+        Long description
+    .EXAMPLE
+        PS C:\> <example usage>
+        Explanation of what the example does
+    .INPUTS
+        Inputs (if any)
+    .OUTPUTS
+        Output (if any)
+    .NOTES
+        General notes
+    #>
+}
+
+function New-Config {
+    <#
+    .SYNOPSIS
+        Creates the config files for the NBR player's conversion system
+    .DESCRIPTION
+        Long description
+    .EXAMPLE
+        PS C:\> <example usage>
+        Explanation of what the example does
+    .INPUTS
+        Inputs (if any)
+    .OUTPUTS
+        Output (if any)
+    .NOTES
+        General notes
+    #>    
+}
+
+function ConvertFrom-ARF {
+    <#
+    .SYNOPSIS
+        Convert the ARF file to the specified type
+    .DESCRIPTION
+        Long description
+    .EXAMPLE
+        PS C:\> <example usage>
+        Explanation of what the example does
+    .INPUTS
+        Inputs (if any)
+    .OUTPUTS
+        Output (if any)
+    .NOTES
+        General notes
+    #>
+}
+
+
+if ($CLIMode) {
+    # Execute script as CLI mode
+}
